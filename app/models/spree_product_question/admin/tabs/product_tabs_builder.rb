0df44cce83375e8f71d9dae19fb7b9a6cc5680f5
Mausam Pun <mausam@truemark.tech>
2024-05-23 21:16:32 +0545

    resolved the comments

--- app/models/spree_product_question/admin/tabs/product_tabs_builder.rb
+++ app/models/spree_product_question/admin/tabs/product_tabs_builder.rb
@@ -11,8 +11,8 @@ module SpreeProductQuestion
         private
 
         def add_questions_tab
-          Spree::Admin::Tabs::TabBuilder.new('question', ->(resource) { admin_product_product_questions_path(resource) }).
-            with_icon_key('question-square.svg').
+          Spree::Admin::Tabs::TabBuilder.new('products_qa.config_name', ->(resource) { admin_product_product_questions_path(resource) }).
+            with_icon_key('question-lg.svg').
             with_active_check.
             with_admin_ability_check(::Spree::ProductQuestion).
             build
