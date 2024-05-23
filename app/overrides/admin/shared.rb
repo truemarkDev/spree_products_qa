3218b315d3e96945a9f0387668f4dea515168837
Mausam Pun <sonicmausam@gmail.com>
2024-05-24 01:56:37 +0545

    add questions tab in product edit page (#21)
    * add questions tab in product edit page

--- app/overrides/admin/shared.rb
+++
@@ -1,9 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_product_tabs',
-                     name: 'add_qa_link',
-                     insert_bottom: '[data-hook="admin_product_tabs"]',
-                     partial: 'spree/admin/shared/additional_product_tabs')
-
-Deface::Override.new(virtual_path: 'spree/admin/shared/_product_sub_menu',
-                     name: "add_qa_tab",
-                     insert_bottom: "[data-hook='admin_product_sub_tabs']",
-                     text: "<%= tab :qna, url: pending_admin_questions_url, label: qna_tab_label, match_path: '/questions/pending' %>")
