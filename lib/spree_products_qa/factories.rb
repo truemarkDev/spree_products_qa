7c28b7e69dc6083273403d1d6557e64d9f30f582
Mausam Pun <mausam@truemark.tech>
2024-05-28 17:58:44 +0545

    WIP: fixes in tests

--- lib/spree_products_qa/factories.rb
+++ lib/spree_products_qa/factories.rb
@@ -1,21 +1,25 @@
-FactoryGirl.define do
-  factory :answer, class: Spree::Answer do
-    content 'Answer'
-  end
+module SpreeProductsQa
+  module Factories
+    FactoryBot.define do
+      factory :answer, class: Spree::ProductAnswer do
+        content 'Answer'
+      end
 
-  factory :question, class: Spree::Question do
-    content 'What is the question?'
-    association :product, factory: :base_product
-    factory :question_with_answer do
-      answer
-    end
-  end
+      factory :question, class: Spree::ProductQuestion do
+        content 'What is the question?'
+        association :product, factory: :base_product
+        factory :question_with_answer do
+          answer
+        end
+      end
 
-  factory :question_answer, class: Spree::ProductQuestion do
-    content 'What is the question?'
-    association :product, factory: :base_product
-    factory :question_with_answer do
-      answer
+      # factory :question_answer, class: Spree::ProductQuestion do
+      #   content 'What is the question ?'
+      #   association :product, factory: :base_product
+      #   factory :question_with_answer do
+      #     answer
+      #   end
+      # end
     end
   end
 end
