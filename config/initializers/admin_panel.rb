4f66fa1f083776dfbec2ad84c3117ade2c3f7e8d
Mausam Pun <mausam@truemark.tech>
2024-05-30 16:56:57 +0545

    API for update and namespace resolution

--- config/initializers/admin_panel.rb
+++ config/initializers/admin_panel.rb
@@ -1,7 +1,7 @@
 Rails.application.config.after_initialize do
   if Spree::Core::Engine.backend_available?
-    Rails.application.config.spree_backend.main_menu.insert_before('users', SpreeProductQuestion::Admin::MainMenu::QuestionBuilder.new.build)
+    Rails.application.config.spree_backend.main_menu.insert_before('users', SpreeProductsQa::Admin::MainMenu::QuestionBuilder.new.build)
 
-    Rails.application.config.spree_backend.tabs[:product].add(SpreeProductQuestion::Admin::Tabs::ProductTabsBuilder.new.build)
+    Rails.application.config.spree_backend.tabs[:product].add(SpreeProductsQa::Admin::Tabs::ProductTabsBuilder.new.build)
   end
 end
