228b856cadf8426b9cbae43895742dd9b8a70bc8
Mausam Pun <mausam@truemark.tech>
2024-06-14 13:26:37 +0545

    Remove worker for mailer and fix its tests

--- spec/models/spree/answer_spec.rb
+++ spec/models/spree/answer_spec.rb
@@ -1,25 +1,30 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Spree::ProductAnswer do
-#   describe '#send_email' do
-#     let(:answer) { build(:answer, question: create(:question)) }
+describe Spree::ProductAnswer do
+  describe '#send_email' do
+    let(:user) { create(:user) }
+    let(:answer) { build(:product_answer, product_question: create(:product_question), user_id: user.id) }
 
-#     it 'should not queue the question' do
-#       expect {
-#         answer.save
-#       }.to_not change(QaMailerWorker.jobs, :size)
-#     end
+    before do
+      allow_any_instance_of(described_class).to receive(:user).and_return(user)
+    end
 
-#     context 'with send_email enabled' do
-#       before(:each) do
-#         allow(SpreeProductsQa).to receive(:send_email?).and_return(true)
-#       end
+    it 'should not queue the question' do
+      expect {
+        answer.save
+      }.not_to have_enqueued_job.on_queue('default')
+    end
 
-#       it 'should queue the question' do
-#         expect {
-#           answer.save
-#         }.to change(QaMailerWorker.jobs, :size).by(1)
-#       end
-#     end
-#   end
-# end
+    context 'with send_email enabled' do
+      before(:each) do
+        allow(SpreeProductsQa).to receive(:send_email?).and_return(true)
+      end
+
+      it 'should queue the question' do
+        expect {
+          answer.save
+        }.to have_enqueued_job.on_queue('default')
+      end
+    end
+  end
+end
