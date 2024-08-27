625760811abcc1620c0b598577737524b158cb42
Mausam Pun <sonicmausam@gmail.com>
2024-08-27 19:09:40 +0545

    added condition for inheriting controller from frontend (#27)
    * Migration for product questions and changes in api for handling unauthorized and unauthorized accesses
    * Admin view for question settings (#24)
    * Unique products in pending questions
    * Add select for user and fixes in prod env
    * Fixes in account qa api and view
    * Added condition for inheriting controller from frontend
    * Regenerate gem with latest config
    * Use ruby 3.3.1 for development
    * fixed old tests and added new ones
    * Remove worker for mailer and fix its tests
    * Change minimum required spree version to 4.7.0
    
    ---------
    
    Co-authored-by: Prakash Poudel <prakash@truemark.com.np>
    Co-authored-by: Prakash <poudelprakash@users.noreply.github.com>

--- app/controllers/spree/product_questions_controller.rb
+++ app/controllers/spree/product_questions_controller.rb
@@ -1,24 +1,33 @@
-class Spree::ProductQuestionsController < Spree::Api::V2::ResourceController
-  before_action :load_data
+module Spree
+  if Spree::Core::Engine.frontend_available?
+    class ProductQuestionsController < StoreController
+      before_action :load_data
 
-  def create
-    question = @product.product_questions.new(allowed_params)
-    question.user_id = current_spree_user.id
-    if question.save
-      flash[:notice] = t('question.sent')
-      redirect_to @product
-    else
-      flash[:error] = question.errors.full_messages.to_sentence
-      redirect_to @product
+      def create
+        question = @product.product_questions.new(allowed_params)
+        question.user_id = current_spree_user.id
+        if question.save
+          flash[:notice] = t('question.sent')
+          redirect_to @product
+        else
+          flash[:error] = question.errors.full_messages.to_sentence
+          redirect_to @product
+        end
+      end
+
+      private
+
+      def load_data
+        @product = Spree::Product.find(allowed_params[:product_id])
+      end
+
+      def allowed_params
+        params.require(:product_question).permit(:content, :product_id)
+      end
     end
-  end
-
-  private
-  def load_data
-    @product = Spree::Product.find(allowed_params[:product_id])
-  end
-
-  def allowed_params
-    params.require(:product_question).permit(:content, :product_id)
+  else
+    class ProductQuestionsController
+      # Dummy class to satisfy Zeitwerk autoloading
+    end
   end
 end
